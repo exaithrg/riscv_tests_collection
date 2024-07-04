57d4e6eec72d71fa98a56538ae69f6efa0a4b7bb
exaithrg <syhaoran@163.com>
2024-07-04 16:54:16 +0800

    tc1

---
+++ tests/custom_addd/main.c
@@ -0,0 +1,11 @@
+
+int main(){
+    int i;
+    int gauss;
+    gauss = 0;
+    for(i=0;i<=100;i++){
+        gauss+=i;
+    }
+    // 5050
+    return gauss;
+}
--- tests/custom_addd/readme.txt
+++
@@ -1,3 +0,0 @@
-This folder contains _boot.s and basic.ld from
-    https://riscvasm.lucasteske.dev/
-
