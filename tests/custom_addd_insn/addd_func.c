a3c1024e839ccef37475c777b3348e149c83748e
exaithrg <syhaoran@163.com>
2024-07-08 14:36:55 +0800

    22

--- tests/custom_addd_insn/addd_func.c
+++ tests/custom_addd_insn/addd_func.c
@@ -2,8 +2,8 @@
 int addd_func(int a1, int a2) {
     int result;
     asm volatile (
-        // "addd %0, %1, %2" : "=r"(result) : "r"(a1), "r"(a2)
-       ".insn r 0x0b, 0x5, 0x55, %0, %1, %2"
+        // .insn r opcode, funct3, funct7, rd, rs1, rs2
+        ".insn r 0x0b, 0x5, 0x55, %0, %1, %2"
              : "=r"(result)
              : "r"(a1), "r"(a2)
     );
